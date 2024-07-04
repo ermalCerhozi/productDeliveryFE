b707030b2e85f059e674e5ca50535ad0817c7b20
ermalCerhozi <ermal.cerhozi3@gmail.com>
2024-07-04 13:54:29 +0200

    add environment variables to git ignore

--- src/app/services/bakery-management-api.service.ts
+++ src/app/services/bakery-management-api.service.ts
@@ -21,59 +21,59 @@ export class BakeryManagementApiService {
 
     //CRUD for users
     createUser(user: UserEntity): Observable<UserEntity> {
-        return this.httpClient.post<UserEntity>(`${this.basePath}users`, user)
+        return this.httpClient.post<UserEntity>(`${this.basePath}/users`, user)
     }
 
     getUsers(): Observable<UserEntity[]> {
-        return this.httpClient.get<UserEntity[]>(`${this.basePath}users`)
+        return this.httpClient.get<UserEntity[]>(`${this.basePath}/users`)
     }
 
     // TODO:take only user
     // TODO: when the logged in user edits its info update teh user info in the local storage
     updateUser(user: UserEntity, result: Partial<UserEntity>): Observable<any> {
         console.log(user)
-        return this.httpClient.patch<UserEntity>(`${this.basePath}users/${user.id}`, result)
+        return this.httpClient.patch<UserEntity>(`${this.basePath}/users/${user.id}`, result)
     }
 
     deleteUser(id: number): Observable<any> {
-        return this.httpClient.delete<any>(`${this.basePath}users/${id}`)
+        return this.httpClient.delete<any>(`${this.basePath}/users/${id}`)
     }
 
     getUser(id: number): Observable<UserEntity> {
-        return this.httpClient.get<UserEntity>(`${this.basePath}users/${id}`)
+        return this.httpClient.get<UserEntity>(`${this.basePath}/users/${id}`)
     }
 
     searchUsers(requestPayload: {
         navigation_context: NavigationContext
     }): Observable<UserResponse> {
         return this.httpClient.post<UserResponse>(
-            `${this.basePath}users/search`,
+            `${this.basePath}/users/search`,
             requestPayload.navigation_context
         )
     }
 
     getSellerUsers(): Observable<UserEntity[]> {
-        return this.httpClient.get<UserEntity[]>(`${this.basePath}users/seller`)
+        return this.httpClient.get<UserEntity[]>(`${this.basePath}/users/seller`)
     }
 
     getClientUsers(): Observable<UserEntity[]> {
-        return this.httpClient.get<UserEntity[]>(`${this.basePath}users/client`)
+        return this.httpClient.get<UserEntity[]>(`${this.basePath}/users/client`)
     }
 
     changeUserPassword(id: number, newPass: changeUserPassword): Observable<any> {
-        return this.httpClient.patch<UserEntity>(`${this.basePath}users/${id}/password`, newPass)
+        return this.httpClient.patch<UserEntity>(`${this.basePath}/users/${id}/password`, newPass)
     }
 
     //CRUD for products
     createProduct(product: ProductEntity): Observable<ProductEntity> {
-        return this.httpClient.post<ProductEntity>(`${this.basePath}products`, product)
+        return this.httpClient.post<ProductEntity>(`${this.basePath}/products`, product)
     }
 
     searchProduct(requestPayload: {
         navigation_context: NavigationContext
     }): Observable<ProductResponse> {
         return this.httpClient.post<ProductResponse>(
-            `${this.basePath}products/search`,
+            `${this.basePath}/products/search`,
             requestPayload.navigation_context
         )
     }
@@ -82,43 +82,43 @@ export class BakeryManagementApiService {
         product: ProductEntity,
         result: Partial<ProductEntity>
     ): Observable<ProductEntity> {
-        return this.httpClient.put<ProductEntity>(`${this.basePath}products/${product.id}`, result)
+        return this.httpClient.put<ProductEntity>(`${this.basePath}/products/${product.id}`, result)
     }
 
     deleteProduct(id: number): Observable<ProductEntity> {
-        return this.httpClient.delete<ProductEntity>(`${this.basePath}products/${id}`)
+        return this.httpClient.delete<ProductEntity>(`${this.basePath}/products/${id}`)
     }
 
     getProduct(id: number): Observable<ProductEntity> {
-        return this.httpClient.get<ProductEntity>(`${this.basePath}products/${id}`)
+        return this.httpClient.get<ProductEntity>(`${this.basePath}/products/${id}`)
     }
 
     getProductPricesByIds(ids: number[]): Observable<{ [key: string]: number }> {
         return this.httpClient.post<{ [key: string]: number }>(
-            `${this.basePath}products/price`,
+            `${this.basePath}/products/price`,
             ids
         )
     }
 
     //CRUD for orders
     createOrder(order: any): Observable<any> {
-        return this.httpClient.post<any>(`${this.basePath}orders`, order)
+        return this.httpClient.post<any>(`${this.basePath}/orders`, order)
     }
 
     getOrders(): Observable<OrderResponse> {
-        return this.httpClient.get<any>(`${this.basePath}orders`)
+        return this.httpClient.get<any>(`${this.basePath}/orders`)
     }
 
     updateOrder(orderId: number, result: Partial<any>): Observable<any> {
-        return this.httpClient.put<any>(`${this.basePath}orders/${orderId}`, result)
+        return this.httpClient.put<any>(`${this.basePath}/orders/${orderId}`, result)
     }
 
     deleteOrder(id: number): Observable<any> {
-        return this.httpClient.delete<any>(`${this.basePath}orders/${id}`)
+        return this.httpClient.delete<any>(`${this.basePath}/orders/${id}`)
     }
 
     getOrder(id: number): Observable<any> {
-        return this.httpClient.get<any>(`${this.basePath}orders/${id}`)
+        return this.httpClient.get<any>(`${this.basePath}/orders/${id}`)
     }
 
     //CRUD for order items
@@ -127,32 +127,32 @@ export class BakeryManagementApiService {
     }): Observable<OrderResponse> {
         console.log(requestPayload)
         return this.httpClient.post<OrderResponse>(
-            `${this.basePath}orders/search`,
+            `${this.basePath}/orders/search`,
             requestPayload.navigation_context
         )
     }
 
     deleteOrderItem(id: number): Observable<any> {
-        return this.httpClient.delete<any>(`${this.basePath}order-items/${id}`)
+        return this.httpClient.delete<any>(`${this.basePath}/order-items/${id}`)
     }
 
     getClientFiltersForOrder(payload: any): Observable<any[]> {
         return this.httpClient.post<UserFiltersResponse[]>(
-            `${this.basePath}users/clients/search`,
+            `${this.basePath}/users/clients/search`,
             payload
         )
     }
 
     getSellerFiltersForOrder(payload: any): Observable<any[]> {
         return this.httpClient.post<UserFiltersResponse[]>(
-            `${this.basePath}users/sellers/search`,
+            `${this.basePath}/users/sellers/search`,
             payload
         )
     }
 
     getProductFiltersForOrder(payload: any): Observable<any[]> {
         return this.httpClient.post<ProductsFiltersResponse[]>(
-            `${this.basePath}products/paginated/search`,
+            `${this.basePath}/products/paginated/search`,
             payload
         )
     }
@@ -160,7 +160,7 @@ export class BakeryManagementApiService {
     //Download orders
     downloadOrdersPdf(requestPayload: { navigation_context: NavigationContext }): Observable<Blob> {
         return this.httpClient.post(
-            `${this.basePath}orders/download`,
+            `${this.basePath}/orders/download`,
             requestPayload.navigation_context,
             {
                 responseType: 'blob',
